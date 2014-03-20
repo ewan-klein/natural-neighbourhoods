24e21df67e7d2045e5d4d2e0c0d10d1f126bf5e3
Ewan Klein <ewan.klein@gmail.com>
2014-03-20 15:26:30 +0000

    better log output

--- postcode2latlong.py
+++ postcode2latlong.py
@@ -1,27 +1,39 @@
+"""
+Convert survey data gridrefs into latlong.
+"""
+
 import csv
 from postcodes import PostCoder
-
+LAST = 3091.0
 
 
 def main():
     csv_in = "Survey Data.csv"
-    csv_out = "nn.csv"
+    
     
     reader = csv.reader(open(csv_in, "rU"))
-    for row in reader:
-        #(record_id, data_source,allocated_NN,postcode, Xcord, Ycord , last_Modified, geography)
+    #(record_id, data_source,allocated_NN,postcode, Xcord, Ycord , last_Modified, geography)
+    reader.next() #skip header
+    new_rows = []
+    for row in reader:       
         data = list(row)[0:4]
         postcode = data[3]
         pc = PostCoder()
-        result = pc.get(postcode)
-        print result['geo']
+        gdict = pc.get(postcode)
+        cur = int(data[0]) 
+        percent = (cur/LAST) * 100
+        print ("%f" % (percent)) + "%"
+        lat = gdict['geo']['lat']
+        lng = gdict['geo']['lng']
+        new_row = data.extend([lat, lng])
+        new_rows.append(new_row)
         
-        #try:
-            #(record_id, data_source,allocated_NN,postcode, Xcord, Ycord , last_Modified, geography) = tuple(row)
-        #except ValueError:
-            #print row
-            
-        #new = ['Record Id','Data Source','Allocated NN','Pcode']
+        
+    with open ('nn.csv', mode='r') as csv_out:
+        writer = csv.writer(csv_out, dialect='excel')
+        for row in new_rows:
+            writer.writerow(row)
+         
 
 if __name__ == "__main__":
     main() 
