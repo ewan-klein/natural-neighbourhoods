7580938022317f0b115dbcd311f5fc0d980bafad
Ewan Klein <ewan.klein@gmail.com>
2014-03-28 13:52:11 +0000

    now outputs json

--- scripts/csv2json.py
+++ scripts/csv2json.py
@@ -1,5 +1,5 @@
 import csv
-import geojson
+import geojson as geo
 
 def normalise(reader):
     new_rows = []
@@ -7,8 +7,40 @@ def normalise(reader):
         nn = row[2]
         if nn == "NN not supplied":
             continue
-        else
-        
+        nns = nn.split('/')
+        if len(nns) > 1:
+            for nn in nns[1:]:
+                row[2] = nn
+                new_rows.append(row)
+        else:
+            new_rows.append(row)
+    return new_rows
+
+def lists2json(data, verbose=False):
+    features = []
+    for l in data:
+        try:
+            (nn_id, src, nn, postcode, lat, lng) = tuple(l)
+        except ValueError:
+            print l
+        lat = float(lat)
+        lng = float(lng)
+        feature = geo.Feature(
+        id = nn_id,
+        geometry = geo.Point((lat, lng)),
+        properties = {"nn": nn, "postcode": postcode}
+        )
+        features.append(feature)
+        if verbose:
+            print geo.dumps(feature)
+    return features
+
+def extract_nns(data):
+    nns = []
+    for l in data:
+        nn = l[2]
+        nns.append(nn)
+    return sorted(set(nns))
 
 def main():
     csv_in = "../survey_data_latlng.csv"       
@@ -16,9 +48,15 @@ def main():
     reader.next()
     
     data = normalise(reader)
+    features = lists2json(data)
     
-    json_out = "survey_data.json"
-
+    with open("../survey_data.json", "w") as json_out:
+        geo.dump(geo.FeatureCollection(features), json_out)
+    
+    nns = extract_nns(data)
+    print len(nns)
+    for nn in nns:
+        print nn
 
 if __name__ == "__main__":
     main() 
