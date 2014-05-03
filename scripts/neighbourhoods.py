a316baecdf15b26c259c586b8a14cb54e40adadc
Ewan Klein <ewan@inf.ed.ac.uk>
2014-05-03 12:41:12 +0200

    Refactoring in order to publish HTML in separate gh-pages branch.

--- scripts/neighbourhoods.py
+++ scripts/neighbourhoods.py
@@ -1,6 +1,13 @@
-"""
-
-"""
+#!/usr/bin/env python
+# Encoding: utf-8
+# ----------------------------------------------------------------------------- 
+# Author: Ewan Klein <ewan@raw-text.io>
+# -----------------------------------------------------------------------------
+# For license information, see LICENSE.txt
+# -----------------------------------------------------------------------------
+# 
+# Script to convert 'natural neigbourhoods' CSV file into a heatmap
+#
 
 from collections import Counter, defaultdict
 import csv
@@ -10,27 +17,33 @@ import shutil
 
 
 CSV_IN = "../nn_data_normalised.csv"
+HTMLDIR = "../natural-neighbourhood-pages"
+INDEX-OUT = "index.html"
+JS-OUT = "../natural-neighbourhood-pages/heatmap-data"
 
 
 class Neighourhoods(object):
-    """"""
+    """
+    Class for building natural neighbourhood heatmap.
+    """
 
     def __init__(self, data_source):                
         """
-        Constructor
+        Constructor: requires a CSV file as argument.
         """
         
         self.data = []
         self.nn_dict = {}
         
         
-    def read(self, data_source):
+    def fromCSV(self, data_source):
         """
-        Read data in from a CSV file
+        Read in coordinate data from a CSV file
         """
         reader = csv.reader(open(data_source, "rU"))
         next(reader)
         self.data = reader
+        
             
     def nn_coords(self):
         """
@@ -44,7 +57,12 @@ class Neighourhoods(object):
             d[nn_name].append(geo)        
         self.nn_dict = d
         
+        
     def uglify(self, name):
+        """
+        Convert a natural neighbourhood name into a standard format for
+        filenames
+        """
         name = name.lower()
         name = name.replace(" ", "_")
         name = name.replace("'", "")
@@ -53,7 +71,11 @@ class Neighourhoods(object):
     
     def dump_address_points(self, subdir, verbose=True):
         """
-        Write a file of coordinates for each neighbourhood.
+        Write a separate file of coordinates for each neighbourhood.
+        
+        The heatmap is built with a separate layer for each neighbourhood, so
+        we need to create a separate file and an associated Javascript
+        variable that is bound to the array of geographical coordinates.
         """
         if os.path.exists(subdir):            
             shutil.rmtree(subdir)
@@ -74,7 +96,7 @@ class Neighourhoods(object):
     
 def main():   
     nns = Neighourhoods(CSV_IN)
-    nns.read(CSV_IN)
+    nns.fromCSV(CSV_IN)
     nns.nn_coords()
     nns.dump_address_points('../heatmap_data')
     
