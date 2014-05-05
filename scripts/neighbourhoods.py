aef5f864b5132c0e22c28aee119c6d6ad811c9cb
Ewan Klein <ewan@inf.ed.ac.uk>
2014-05-05 13:19:51 +0200

    add neighbourhoods as objects

--- scripts/neighbourhoods.py
+++ scripts/neighbourhoods.py
@@ -18,7 +18,7 @@ import os
 import shutil
 from string import Template
 
-#import numpy as np
+import numpy as np
 
 
 
@@ -27,6 +27,7 @@ CSV_IN = "../nn_data_normalised.csv"
 HTMLDIR = "../natural-neighbourhoods-pages"
 INDEX_OUT = "../../natural-neighbourhoods-pages/index.html"
 JS_OUT = "../../natural-neighbourhoods-pages/heatmap-data"
+COLOURS = "nn_colours.csv"
 
 HTML = """\
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
@@ -68,11 +69,23 @@ $js_layers
 </script>
 """
 
+class Neighbourhood(object):
+    def __init__(self, name):                
+            """
+            Constructor: requires a CSV file as argument.
+            """
+            
+            self.name = name
+            self.colour = None
+            self.centroid = None
+            self.coords = []  
+    
 
 
-class Neighourhoods:
+
+class Builder(object):
     """
-    Class for building natural neighbourhood heatmap.
+    Class for building natural neighbourhood visualisations.
     """
 
     def __init__(self, data_source):                
@@ -81,10 +94,14 @@ class Neighourhoods:
         """
         
         self.data = []
-        self.nn_dict = {}
+        self.nn_dict = None
+        self.nns = []
         self.nn_names = []
+        self.colours = None
+        
         self.fromCSV(data_source)
-        self.nn_coords()
+        self.neighbourhoods()
+        
         
         
     def fromCSV(self, data_source, verbose=True):
@@ -98,12 +115,11 @@ class Neighourhoods:
         self.data = reader
         
             
-    def nn_coords(self, verbose=True):
+    def add_points(self, verbose=True):
         """
         Build a dictionary that maps neighbourhood names into a list of
         coordinates, one for each survey response.
-        """
-        
+        """      
         d = defaultdict(list)
         for line in self.data:
             geo = tuple(map(float, line[4:6]))
@@ -112,7 +128,25 @@ class Neighourhoods:
         self.nn_dict = d
         self.nn_names = sorted(self.nn_dict.keys())
         if verbose:
-            print("Building dictionary with %s keys\n" % len(d))        
+            print("Building dictionary with %s keys\n" % len(d)) 
+            
+    def neighbourhoods(self):
+        """
+        Convert dictionary entries into Neighbourhood objects.
+        """
+        if self.nn_dict is None:
+            self.add_points()
+        if self.colours is None:
+            self.colours_fromCSV(COLOURS)
+        for name in self.nn_dict:
+            nn = Neighbourhood(name)
+            nn.coords = self.nn_dict[name]
+            nn.centroid = tuple(np.mean(nn.coords, axis=0))
+            if self.colours[nn.name] is not None:
+                nn.colour = Color(self.colours[nn.name])
+            else:
+                nn.colour = Color(pick_for=nn.name)
+            self.nns.append(nn)
         
         
     def standardise(self, name):
@@ -124,6 +158,20 @@ class Neighourhoods:
         name = name.replace(" ", "_")
         name = name.replace("'", "")
         return name
+    
+    
+    def colours_fromCSV(self, fn):
+        d = {}
+        with open(fn) as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    (name, freq, colour) = row
+                    name = self.standardise(name)
+                    if colour == 'None':
+                        colour = None
+                    d[name] = colour
+        self.colours = d
+        
         
     
     def dump_address_points(self, subdir, verbose=True):
@@ -139,15 +187,25 @@ class Neighourhoods:
         os.makedirs(subdir)
         count = 1
         
-        for nn in self.nn_dict:
-            vals = self.nn_dict[nn]          
-            fn = os.path.join(subdir, nn + '.js')
-            header = ("var %s = \n" % nn)
+        for nn in self.nns:
+            vals = nn.coords
+            fn = os.path.join(subdir, nn.name + '.js')
+            header = ("var %s = \n" % nn.name)
             contents = header + json.dumps(vals)
             with open(fn, "w") as outfile:
                 outfile.write(contents)
                 print("[%s] Writing to %s" % (count,fn))
-            count = count + 1
+            count = count + 1            
+        
+        #for nn in self.nn_dict:
+            #vals = self.nn_dict[nn]          
+            #fn = os.path.join(subdir, nn + '.js')
+            #header = ("var %s = \n" % nn)
+            #contents = header + json.dumps(vals)
+            #with open(fn, "w") as outfile:
+                #outfile.write(contents)
+                #print("[%s] Writing to %s" % (count,fn))
+            #count = count + 1
             
 
     def heat_params(self, nn):
@@ -157,7 +215,7 @@ class Neighourhoods:
         d = {}
         d['blur'] = 15
         d['radius'] = 28
-        c = Color(pick_for=nn)
+        c = nn.colour
         c.set_saturation(1)
         
         d['gradient'] = {} 
@@ -175,13 +233,13 @@ class Neighourhoods:
         Create a string representation of all the JS headers required.
         """        
         header_tmpl = '<script src="heatmap-data/%s.js" type="text/javascript"></script>'
-        headers = [(header_tmpl % nn) for nn in self.nn_names]
+        headers = [(header_tmpl % nn.name) for nn in self.nns]
         headers = "\n".join(headers)
         return headers
     
     def js_layers(self):
         layer_tmpl = """    var heatLayer%s = L.heatLayer(%s, %s).addTo(map);"""
-        layers = [(layer_tmpl % (nn.capitalize(), nn, self.heat_params(nn))) for nn in self.nn_names]
+        layers = [(layer_tmpl % (nn.name.capitalize(), nn.name, self.heat_params(nn))) for nn in self.nns]
         layers = "\n".join(layers)
         return layers
     
@@ -203,7 +261,7 @@ class Neighourhoods:
         
     
 def main():   
-    nns = Neighourhoods(CSV_IN)
+    nns = Builder(CSV_IN)
     nns.dump_address_points(JS_OUT)
     nns.build_html(INDEX_OUT)
 
